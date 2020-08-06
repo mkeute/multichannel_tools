bdc1dfdcb279d462ddf647ed19ab775ca2d0203f
Marius Keute <marius@marius.speedport.ip>
2020-08-06 21:03:22 +0200

    add utils module

---
+++ multichannel_tools/utils.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+import math
+def find_nearest(array,value):
+    idx = np.searchsorted(array, value, side="left")
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+        return idx-1
+    else:
+        return idx
+
+
