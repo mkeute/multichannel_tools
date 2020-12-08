07987ea37b1562a87284cd58943c5c25d73fdd0d
mkeute <marius.keute@gmail.com>
2020-12-08 08:41:48 +0100

    Add pupil residualization function

--- multichannel_tools/utils.py
+++ multichannel_tools/utils.py
@@ -2,6 +2,9 @@
 import numpy as np
 import math
 from scipy import signal
+from sklearn.decomposition import PCA
+from statsmodels.regression.linear_model import OLS
+
 def find_nearest(array,value):
     idx = np.searchsorted(array, value, side="left")
     if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
@@ -47,7 +50,39 @@ def common_avg_ref(dat):
 def TKEO(x):
     y = x[1:-1]**2 - x[:-2]*x[2:]
     
-    y = np.array([y[0],y,y[-1]])
+    y = np.concatenate([[y[0]],y,[y[-1]]])
     
     return y
         
+
+
+
+
+
+
+def regress_out_pupils(raw, ocular_channels = ['Fpz', 'Fp1', 'Fp2', 'AF7', 'AF8'], method = 'PCA'):
+    
+    """
+    raw: Continuous raw data in MNE format
+    ocular_channels: can be labels of EOG channels or EEG channels close to the
+        eyes if no EOG was recorded
+    method: how to combine the ocular channels. Can be 'PCA', 'mean', or 'median'.
+    """
+    
+    raw_data = raw.get_data(picks = 'eeg')
+    ocular_data = raw.get_data(picks = ocular_channels)
+    
+    if method == 'PCA':
+        pca = PCA()
+        comps = pca.fit_transform(ocular_data.T)
+        ocular_chan = comps[:,0]
+    elif method == 'mean':
+        ocular_chan = np.mean(ocular_data, axis = 0)
+    elif method == 'median':
+        ocular_chan = np.median(ocular_data, axis = 0)
+    
+    for ch in range(raw_data.shape[0]):
+        m = OLS(raw_data[ch,:], ocular_chan)
+        raw_data[ch,:] -= m.fit().predict()
+    raw._data[:raw_data.shape[0],:] = raw_data
+    return raw
